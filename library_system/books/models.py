2a0d4ad31a5be3cfa7869509f722685d8825362b
BILLY <104698069+beanboi64@users.noreply.github.com>
2026-06-22 23:29:32 +0200

    refactor: fix model relationships and add __str__ methods

--- library_system/books/models.py
+++ library_system/books/models.py
@@ -2,15 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Author(models.Model):
-    first_name = models.CharField(max_length=255, blank=False, verbose_name="First Name")
-    last_name = models.CharField(max_length=255, blank=False, verbose_name="Last Name")
-    date_of_birth = models.DateField(auto_now=True)
-    date_of_death = models.DateField(auto_now=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
 
 class Book(models.Model):
-    title = models.CharField(max_length=255, blank=False, verbose_name="Book Title")
-    summary = models.TextField(max_length=255, blank=True, verbose_name="Book Summary")
+    title = models.CharField(max_length=255)
+    summary = models.TextField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 class BookInstance(models.Model):
     RETURNED = 'RET'
@@ -24,6 +30,6 @@ class BookInstance(models.Model):
     }
 
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    imprint = models.ForeignKey(Author, on_delete=models.CASCADE)
-    due_back = models.DateField(auto_now=True)
+    imprint = models.CharField(Author, on_delete=models.CASCADE)
+    due_back = models.DateField(null=True, blank=True)
     status = models.CharField(max_length=3, choices=STATUS_CHOICES, default=RETURNED)
