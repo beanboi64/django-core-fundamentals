aeff35eb1a8cc88f57365b18ef160c155a430511
BILLY <104698069+beanboi64@users.noreply.github.com>
2026-06-22 23:11:54 +0200

    setup: initialize books app and define ORM models for library management

--- library_system/books/models.py
+++ library_system/books/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=255, blank=False, verbose_name="First Name")
+    last_name = models.CharField(max_length=255, blank=False, verbose_name="Last Name")
+    date_of_birth = models.DateField(auto_now=True)
+    date_of_death = models.DateField(auto_now=True)
+
+class Book(models.Model):
+    title = models.CharField(max_length=255, blank=False, verbose_name="Book Title")
+    summary = models.TextField(max_length=255, blank=True, verbose_name="Book Summary")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+class BookInstance(models.Model):
+    RETURNED = 'RET'
+    BORROWED = 'BOR'
+    OVERDUE = 'DUE'
+
+    STATUS_CHOICES = {
+        RETURNED: 'returned',
+        BORROWED: 'borrowed',
+        OVERDUE: 'overdue'
+    }
+
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    imprint = models.ForeignKey(Author, on_delete=models.CASCADE)
+    due_back = models.DateField(auto_now=True)
+    status = models.CharField(max_length=3, choices=STATUS_CHOICES, default=RETURNED)
